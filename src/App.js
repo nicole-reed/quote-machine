6e818c08283646e48324282bf3c38ee75aebb3aa
Nicole Reed <nickreed033@gmail.com>
2020-10-15 16:13:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-function App() {
+
+const App = () => {
+
+  const [quote, setQuote] = useState({});
+
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getQuote = async () => {
+    try {
+      const response = await fetch(`http://quotes.stormconsultancy.co.uk/random.json`)
+      const data = await response.json();
+      console.log(data)
+      setQuote({ author: data.author, quote: data.quote })
+
+    } catch (error) {
+      console.log('error fetching quote:\n', error)
+
+    }
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 id='title'>Random Quotes</h1>
+      <div id='quote-box'>
+        <p id='text'>"{quote.quote}"</p>
+        <p id='author'>-{quote.author}</p>
+        <div id='clickables'>
+
+          <a id='tweet-quote' href="twitter.com/intent/tweet">
+            <FontAwesomeIcon icon={faTwitter} />
+          share this wisdom on twitter</a>
+          <button id='new-quote' onClick={getQuote}>Get A New Quote</button>
+        </div>
+
+
+
+      </div>
     </div>
   );
 }
