5bcf46157756792b077d5a5c4c8b0811f66d79bf
Nicole Reed <nickreed033@gmail.com>
2020-10-15 16:21:23 +0300

    Updates

---
+++ precache-manifest.c4ffd5aa2620648a6a5e7b6536401b33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284d72060ed27611a36730098d049dbb",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "208302d08d29b7df1b31",
+    "url": "/quote-machine/static/css/main.a7941caa.chunk.css"
+  },
+  {
+    "revision": "8e61c7d3d7cf4a9057f8",
+    "url": "/quote-machine/static/js/2.9a64c42a.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/quote-machine/static/js/2.9a64c42a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208302d08d29b7df1b31",
+    "url": "/quote-machine/static/js/main.00889585.chunk.js"
+  },
+  {
+    "revision": "256db52c5a0c9de87c09",
+    "url": "/quote-machine/static/js/runtime-main.6ade76f3.js"
+  }
+]);
